4ceb9160811171d02e3828dde2af13aa3f057c93
rohanghosh <rohangho@live.com>
2022-11-10 23:48:15 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Demo App 1'),);
+  }
+
+
+}
