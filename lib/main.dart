56f6eb060c08605ba91c84691949ecc741cc8eca
rohanghosh <rohangho@live.com>
2022-11-16 01:53:19 +0530

    add method call in button pressa

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  void onButtonPressed(){
+    print("Answered");
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text("My First App")),
             body: Column(
-              children: const <Widget>[
+              children:  <Widget>[
                 Text("My app bar body"),
-                RaisedButton(child: Text("PressME"), onPressed: null),
-                RaisedButton(child: Text("PressME"), onPressed: null),
-                RaisedButton(child: Text("PressME"), onPressed: null)
+                RaisedButton(child: Text("PressME"), onPressed: onButtonPressed),
+                RaisedButton(child: Text("PressME"), onPressed: onButtonPressed),
+                RaisedButton(child: Text("PressME"), onPressed: onButtonPressed)
               ],
             )));
   }
