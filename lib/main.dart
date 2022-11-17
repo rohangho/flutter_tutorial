baf97a2ca1818f5ca7d40ce62e963c64ccffd798
rohanghosh <rohangho@live.com>
2022-11-18 01:20:13 +0530

    added the stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
 
-class MyApp extends StatelessWidget {
-
-  void onButtonPressed(){
-    print("Answered");
+class AppState extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
+}
+
+class MyAppState extends State<AppState> {
+  var questionIndex = 0;
+  void onButtonPressed() {
+    print("Error Name");
+  }
+
 
 
   @override
   Widget build(BuildContext context) {
+    var question = ["My name is?", "My Age is"];
+
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text("My First App")),
             body: Column(
-              children:  <Widget>[
-                Text("My app bar body"),
-                RaisedButton(child: Text("PressME"), onPressed: onButtonPressed),
-                RaisedButton(child: Text("PressME"), onPressed: onButtonPressed),
-                RaisedButton(child: Text("PressME"), onPressed: onButtonPressed)
+              children: <Widget>[
+                Text(question[questionIndex]),
+                RaisedButton(
+                    child: Text("Jadu"), onPressed: onButtonPressed),
+                RaisedButton(
+                    child: Text("Rohan"),
+                    onPressed: () {
+                      setState(() {
+                        questionIndex += 1;
+                      });
+                      print(questionIndex);
+                    }),
+                RaisedButton(child: Text("Madu"), onPressed: onButtonPressed)
               ],
             )));
   }
